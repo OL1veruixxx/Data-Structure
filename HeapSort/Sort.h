50a31f97a6b695ba39303129c4fe5a2e9ad1f4a5
OL1verui <106242212+OL1veruixxx@users.noreply.github.com>
2022-08-09 23:58:36 +0800

    Add files via upload

---
+++ HeapSort/Sort.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
+void PrintArray(int* a, int n);
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
+void HeapSort(int* a, int n);
